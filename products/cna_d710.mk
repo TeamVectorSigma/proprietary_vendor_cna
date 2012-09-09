ab1847637ef64f9bfc56412fc043f1782beba1ec
justlove joyous <justlovejoy@gmail.com>
2012-09-09 15:30:44 -0400

    noob derps again

--- products/cna_d710.mk
+++ products/cna_d710.mk
@@ -3,14 +3,11 @@ $(call inherit-product, device/samsung/d710/full_d710.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/cna/config/common_full_phone.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/cna/config/cdma.mk)
-
 PRODUCT_COPY_FILES +=  \
     vendor/cna/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
 # Release name
-PRODUCT_RELEASE_NAME := SPH-D710
+PRODUCT_RELEASE_NAME := d710
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D710 TARGET_DEVICE=SPH-D710 BUILD_FINGERPRINT=samsung/SPH-D710/SPH-D710:4.0.4/IMM76I/FF18:user/release-keys  PRIVATE_BUILD_DESC="SPH-D710-user 4.0.4 IMM76I FF18 release-keys
 
